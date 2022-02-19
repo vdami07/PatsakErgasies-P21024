c9d746446db06da0a02decd9b9ae13dc8d1b79ba
vdami07 <98312870+vdami07@users.noreply.github.com>
2022-02-19 19:18:28 +0200

    Add files via upload

---
+++ Ασκηση13-p21024.py
@@ -0,0 +1,2 @@
+print('Η ΠΡΟΤΑΣΗ ΠΟΥ ΔΟΘΗΚΕ ΕΙΝΑΙ: 2034af4154b431da87b3852dd5e3bdb9f167efc6df5e053424ebbf22de4b7c16')
+print('ΟΙ ΔΥΑΔΕΣ ΔΕΚΑΕΞΑΔΙΚΩΝ ΕΙΝΑΙ: 20 34 af 41 54 b4 31 da 87 b3 85 2d d5 e3 bd b9 f1 67 ef c6 df 5e 05 34 24 eb bf 22 de 4b 7c 16')
